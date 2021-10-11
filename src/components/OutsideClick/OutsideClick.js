7a700532ed5a2076efb69ff97b67bd513576aab9
ChrisCooper0 <christopher.cooper@mail.com>
2021-10-11 20:28:17 +0100

    fix outside click

--- src/components/OutsideClick/OutsideClick.js
+++ src/components/OutsideClick/OutsideClick.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import OutsideClickHandler from "react-outside-click-handler";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const OutsideClick = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,20 +10,48 @@ const OutsideClick = () => {
     setIsOpen(!isOpen);
   };
 
+  const StyledWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  `;
+
+  const StyledButton = styled.button`
+    border: none;
+    outline: none;
+    padding: 10px 8px;
+    border-radius: 4px;
+    background-color: lightblue;
+    color: #000;
+    cursor: pointer;
+    font-size: 16px;
+    margin-bottom: 2rem;
+  `;
+
+  const StyledDiv = styled.div`
+    background-color: lightblue;
+    padding: 2rem;
+  `;
+
   return (
-    <div>
+    <>
       <Link className="link" to="/">
         Back
       </Link>{" "}
-      <OutsideClickHandler onOutsideClick={handleClick}>
-        <button onClick={handleClick}>{isOpen ? "Close" : "Open"}</button>
-      </OutsideClickHandler>
-      {isOpen && (
-        <div className="card">
-          <h3>Click outside to close</h3>
-        </div>
-      )}
-    </div>
+      <StyledWrapper>
+        <StyledButton onClick={handleClick}>
+          {isOpen ? "Close" : "Open"}
+        </StyledButton>
+        <OutsideClickHandler onOutsideClick={handleClick}>
+          {isOpen && (
+            <StyledDiv>
+              <h3>Click outside this box to close</h3>
+            </StyledDiv>
+          )}
+        </OutsideClickHandler>
+      </StyledWrapper>
+    </>
   );
 };
 
